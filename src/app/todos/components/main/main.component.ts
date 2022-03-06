3153b4f769069ca2fa3d86d4a0a802e8d3133e92
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-03-06 16:59:25 +0300

    reload

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,8 +1,25 @@
 import { Component } from "@angular/core";
+import { filter, Observable } from "rxjs";
+import { combineLatestInit } from "rxjs/internal/observable/combineLatest";
+import { TodosService } from "../../services/todos.service";
+import { FilterEnum } from "../todos/types/filter.enum";
+import { TodoInterface } from "../todos/types/todo.interface";
 
 
 @Component({
     selector: 'app-todos-main',
     templateUrl: './main.component.html'
 })
-export class MainComponent{}
+export class MainComponent{
+   visibleTodos$:Observable<TodoInterface[]>;
+   constructor (private todosService: TodosService){
+       this.visibleTodos$ = combineLatest(
+           this.todosService.todos$,
+           this.todosService.filter$,
+       ).pipe(map(([todos, filter]: [TodoInterface[],FilterEnum])=>{
+           console.log('combine', todos, filter);
+           return[];
+       }
+       ));
+   }
+}
