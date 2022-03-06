9d0c59248f6d7e701d5781cdcaf15d7aa64d2569
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-03-06 03:01:16 +0300

    adding

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TodosComponent } from "src/app/todos/components/todos/todos.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 const routes: Routes =[
     {
@@ -11,7 +12,7 @@ const routes: Routes =[
 
 
 @NgModule({
-    declarations:[TodosComponent],
+    declarations:[TodosComponent, HeaderComponent],
     imports:[RouterModule.forChild(routes)]
 })
 export class TodosModule{}
