cb437c444de75066df41d627373232a95e4fc0b1
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-03-08 21:33:34 +0300

    reload

---
+++ src/app/todos/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-todos-footer',
+    templateUrl: 'app/footer.component.html',
+  })
+  export class FooterComponent{
+
+  }
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,29 +1,30 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TodosComponent } from "src/app/todos/components/todos/todos.component";
-import { HeaderComponent } from "./components/header/header.component";
-import { MainComponent } from "./components/main/main.component";
-import { TodoComponent } from "./components/todo/todo.component";
-import { TodosService } from "./services/todos.service";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TodosService } from './services/todos.service';
+import { MainComponent } from './components/main/main.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: TodosComponent,
-    }
-]
-
+  {
+    path: '',
+    component: TodosComponent,
+  },
+];
 
 @NgModule({
-    declarations: [
-      TodosComponent,
-      HeaderComponent,
-      MainComponent,
-      TodoComponent
-    ],
-    imports: [CommonModule, RouterModule.forChild(routes)],
-    providers: [TodosService],
-  })
-  export class TodosModule {}
+  declarations: [
+    TodosComponent,
+    HeaderComponent,
+    MainComponent,
+    TodoComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  providers: [TodosService],
+})
+export class TodosModule {}
